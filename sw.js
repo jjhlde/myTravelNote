22eb93ae59554424f228eebbadfa5d90355fe666
Jungho Jang <jjhlde@naver.com>
2025-07-18 08:15:21 +0900

    sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'macau-travel-v1';
+const urlsToCache = [
+  '/myTravelNote/',
+  '/myTravelNote/index.html',
+  '/myTravelNote/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 캐시에서 찾으면 반환, 없으면 네트워크 요청
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
